508b6d95452ca240960f3613424d604196e8b68b
Rikishi_02 <84096779+Rikishi1742@users.noreply.github.com>
2023-03-03 12:10:49 +0300

    Add files via upload

--- src/components/Expense/ExpensesList.js
+++ src/components/Expense/ExpensesList.js
@@ -5,7 +5,7 @@ import './ExpensesList.css';
 const ExpensesList = (props) => {
 
     if (props.items.length === 0) {
-        return <h2 className="expenses-list__fallback">Found no expenses.</h2>
+        return <h3 className="expenses-list__fallback">Found no expenses.</h3>
     }
 
     return (
